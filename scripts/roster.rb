6ee0b5af4999ec6627d857137b7eda4658ad5f08
bergren2 <dan@heymrbass.com>
2014-09-02 14:07:59 -0500

    replace ', ' delimiters with just ','
    We should be stricter about CSVs anyway.

--- scripts/roster.rb
+++ scripts/roster.rb
@@ -4,7 +4,7 @@ puts "Enter team:"
 team = gets.strip.upcase
 players = []
 # determines roster of a team given the list of transactions
-CSV.foreach(File.expand_path("../../seasons/2014_transactions.csv", __FILE__), headers: true, col_sep: ', ') do |csv|
+CSV.foreach(File.expand_path("../../seasons/2014_transactions.csv", __FILE__), headers: true) do |csv|
   if csv['Team'].eql? team
     if csv['Type'].eql? 'Add'
       players.push csv['Player']
